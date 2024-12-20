f54247b66d5e3341d5501a059f1b6bc2c0e4d75f
HelioMelo <Helio_melo_@hotmail.com>
2024-12-20 15:13:13 -0300

    feat(categories): add check for category existence

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,7 +5,13 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository
 
 categoriesRoutes.post("/", (request, response)=>{
+    
     const {name, description} = request.body;
+
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if(categoryAlreadyExists){
+        return response.status(400).json({error: "Category already exists"})
+    }
 
     categoriesRepository.create({name, description})
 
